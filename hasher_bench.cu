e3d53b273a2935325d0da4236e2988e0354e7124
Dave Andersen <dga@cs.cmu.edu>
2013-12-16 10:23:02 -0500

    Stripping out some unused old code for clarity.

--- hasher_bench.cu
+++ hasher_bench.cu
@@ -48,44 +48,6 @@ int main() {
   int rc = h->ScanNCoins(job, target, n_attempts, &stop, NULL);
   gettimeofday(&tv_end, NULL);
 
-#if 0
-    /* Note:  This code has bit-rotted.  N is no longer defined.
-    * Change to use GetBatchSize() if you want to run it more. */
-  scrypt_hash *keys_in = (scrypt_hash *)malloc(N*sizeof(scrypt_hash));
-  if (!keys_in) {
-    exit(-1);
-  }
-  scrypt_hash *keys_out = (scrypt_hash *)malloc(N*sizeof(scrypt_hash));
-  if (!keys_out) {
-    free(keys_out);
-    exit(-1);
-  }
-
-  printf("Host malloc complete: %ld bytes\n", N*sizeof(scrypt_hash));
-  fflush(stdout);
-
-  init_test_keys(keys_in, h.GetBatchsize()*100);
-  gettimeofday(&tv_end, NULL);
-
-  for (int i = 0; i < BENCHMARK_LOOPS; i++) { // benchmark loop
-    h->ComputeHashes(keys_in, keys_out, N);
-  }
-  for (int key = 0; key < 2; key++) {
-#if 1
-  for (int i = 0; i < 16; i++) {
-    printf("%8.8x ", keys_out[key].b[i]);
-    if (i%8 == 7) { printf("\n"); }
-  }
-  for (int i = 0; i < 16; i++) {
-    printf("%8.8x ", keys_out[key].bx[i]);
-    if (i%8 == 7) { printf("\n"); }
-  }
-
-  printf("\n");
-  }
-#endif
-#endif
-
   double n_sec = timeval_diff(&tv_start, &tv_end);
 
   printf("%d hashes in %2.2f seconds (%2.2f kh/s)\n", 
